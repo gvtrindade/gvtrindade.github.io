60c39f3aab2c08512ca90c8d790d2f93c75e635d
Gabriel Trindade <gabrielsvtrindade@gmail.com>
2024-10-03 14:33:22 -0300

    Atualizar texto do site

--- assets/texts/Textos.js
+++ assets/texts/Textos.js
@@ -9,7 +9,7 @@ const textoPt = {
   descritivo__titulo: "Sobre Mim",
   descritivo__texto1: `${contadorDeAnos()} anos de experiência com Javascript. Atualmente trabalha como estagiário na Basis Tecnologia. Não consegue trabalhar sem música.`,
   descritivo__texto2: "Sites com design moderno e responsívo, PWAs e aplicações web.",
-  descritivo__texto3: "Bootsrap, PrimeNG e CSS puro. Implementações simples de back-end com Java e Firebase.",
+  descritivo__texto3: "Habilidades em React e Angular, com implementações de backend em Java e Python. Testes unitários, CI/CD, gerenciamento de bancos de dados",
   qualificacoes__titulo: "Qualificações",
   projetos__titulo: "Projetos",
   inicioTimeline: "Início"
@@ -18,9 +18,9 @@ const textoPt = {
 const textoEn = {
   inicio__texto__subtexto: "web developer, studying for a system development and analysis degree.",
   descritivo__titulo: "About me",
-  descritivo__texto1: `${contadorDeAnos()} years of experience with Javascript. Currently works as an intern at Basis Tecnologia. Can't work without music.`,
+  descritivo__texto1: `${contadorDeAnos()} years of experience with Javascript. Can't work without music.`,
   descritivo__texto2: "Websites with modern and responsive design, PWAs and web apps.",
-  descritivo__texto3: "Bootsrap, PrimeNG e pure CSS puro. Simple back-end implementations with Java and Firebase.",
+  descritivo__texto3: "Mastering React and Angular, with backend implementations in Java and Python. Unit tests, CI/CD, database management",
   qualificacoes__titulo: "Skills",
   projetos__titulo: "Projects",
   inicioTimeline: "Begining"
